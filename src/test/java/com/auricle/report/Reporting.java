8cc573897b5a3c5550300b11c5f66e09a5cb99c1
thivyaprakas24 <thivyaprakas24@gmail.com>
2026-05-24 13:26:00 +0530

    Added Code from Tester1

---
+++ src/test/java/com/auricle/report/Reporting.java
@@ -0,0 +1,20 @@
+package com.auricle.report;
+
+public class Reporting {
+
+	private void jvmReport() {
+		System.out.println("POSTMAN");
+
+	}
+
+	private void allureReport() {
+		System.out.println("APPIUM");
+
+	}
+
+	private void extendsReport() {
+		System.out.println("SELENIUM");
+
+	}
+
+}
